3868bbaf3705ec74dee5e563e877a56155b445c2
Omid Safarzadeh <omidpoly@gmail.com>
2019-04-15 15:50:50 +0300

    Update guardian.R

--- guardian.R
+++ guardian.R
@@ -3,6 +3,6 @@ library(GuardianR)
 results <- get_guardian(keywords="spycops",
                         from.date="2018-06-01",
                         to.date="2018-06-08",
-                        api.key="a538ddb1-f598-4ffa-abce-bd2e5fc5583d")
+                        api.key="?????????????")
 
 results$headline
